9cbcfea03404eb4d20b7cab501dde1350b22a662
Jorge Morales <luifer991@gmail.com>
2024-08-12 10:46:43 -0500

    adding background styles and elements

--- src/UI/Footer.tsx
+++ src/UI/Footer.tsx
@@ -1,9 +1,35 @@
 import zorvanzLogo from '/ZorvanzZorvanz.svg'
+import '../components/styles/footer.css'
 
 export default function Footer() {
     return (
         <footer>
-            <img src={zorvanzLogo} alt="zorvanz logo" />
+            <section className='footer__logo'>
+                <img src={zorvanzLogo} alt="zorvanz logo" />
+            </section>
+            <section className='footer__info'>
+                <div>
+                    <p>Lorem, ipsum dolor.</p>
+                    <p>Lorem, ipsum dolor.</p>
+                    <p>Lorem, ipsum dolor.</p>
+                </div>
+                <div>
+                    <p>Lorem, ipsum dolor.</p>
+                    <p>Lorem, ipsum dolor.</p>
+                    <p>Lorem, ipsum dolor.</p>
+                </div>
+                <div>
+                    <p>Lorem, ipsum dolor.</p>
+                    <p>Lorem, ipsum dolor.</p>
+                    <p>Lorem, ipsum dolor.</p>
+                </div>
+                <div>
+                    <p>Lorem, ipsum dolor.</p>
+                    <p>Lorem, ipsum dolor.</p>
+                    <p>Lorem, ipsum dolor.</p>
+                    <p>Lorem, ipsum dolor.</p>
+                </div>
+            </section>
         </footer>
     )
 }
