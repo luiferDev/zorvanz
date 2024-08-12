542e72bbb312a948032323ecffde7b7dd0f16352
Jorge Morales <luifer991@gmail.com>
2024-08-12 17:55:39 -0500

    change form to info and adding styles

--- src/components/ContactsFrom.tsx
+++ src/components/ContactsFrom.tsx
@@ -1,16 +1,16 @@
+import '../components/styles/Zorvanz.css'
 
-export default function ContactsFrom() {
+export default function Zorvanz() {
     return (
         <>
-            <form action="" method="post">
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
-                <label htmlFor="message">Message</label>
-                <textarea name="message" id="message"></textarea>
-                <button type="submit">Submit</button>
-            </form>
+            <section className='zorvanz__info'>
+                <h2>Conoce Zorvanz</h2>
+                <p>Zorvanz es una marca dedicada a crear experiencias olfativas únicas. Con una amplia gama de productos que incluyen perfumes personalizados, velas aromáticas y todo lo relacionado con los aromas, cada creación es un reflejo de la calidad y la pasión por la excelencia.
+                </p>
+                <p>
+                    Nos enfocamos en personalizar cada fragancia, permitiendo a nuestros clientes expresar su identidad a través de aromas únicos. En Zorvanz, creemos que cada persona es única y merece una fragancia que lo sea también. ¡Descubre la esencia que te define!
+                </p>
+            </section>
         </>
     )
 }
