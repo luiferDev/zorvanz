ef9b0f5a66dd8805050411c75c20ad14a0522e6e
Jorge Morales <luifer991@gmail.com>
2024-08-13 14:13:33 -0500

    adding error page for the routing

---
+++ src/error-page.tsx
@@ -0,0 +1,29 @@
+import { isRouteErrorResponse, Link, useRouteError } from "react-router-dom";
+import './components/styles/styles.css'
+
+export default function ErrorPage() {
+    const error = useRouteError();
+
+    if (isRouteErrorResponse(error)) {
+        console.error(error);
+
+        return (
+            <div id="error-page">
+                <h1><Link to="/">Oops!</Link></h1>
+                <p>Sorry, an unexpected error has occurred.</p>
+                <p>
+                    <i>{error.statusText || error.data?.message}</i>
+                </p>        
+            </div>
+        );
+    } else {
+        console.error("An unknown error occurred");
+
+        return (
+            <div id="error-page">
+                <h1>Oops!</h1>
+                <p>Sorry, an unexpected error has occurred.</p>
+            </div>
+        );
+    }
+}
