511b8000c9cf01e807529c1e3ae678fadd6cc7ba
Jorge Morales <luifer991@gmail.com>
2024-08-13 11:40:56 -0500

    footer styles and refactoring TODO: apply react router for the nav bar

--- src/UI/Footer.tsx
+++ src/UI/Footer.tsx
@@ -6,29 +6,48 @@ export default function Footer() {
         <footer>
             <section className='footer__logo'>
                 <img src={zorvanzLogo} alt="zorvanz logo" />
+                <div>
+                    <p><strong>Dirección: </strong>cll 100</p>
+                    <p><strong>Contacto: </strong>+573186922713</p>
+                    <strong>Siguenos:</strong>
+                </div>
+                <div>
+                    <a href=""><img src="" alt="" /></a>
+                    <a href=""><img src="" alt="" /></a>
+                    <a href=""><img src="" alt="" /></a>
+                </div>
             </section>
             <section className='footer__info'>
                 <div>
-                    <p>Lorem, ipsum dolor.</p>
-                    <p>Lorem, ipsum dolor.</p>
-                    <p>Lorem, ipsum dolor.</p>
+                    <div>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                    </div>
+                    <div>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                    </div>
+                    <div>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                    </div>
                 </div>
                 <div>
-                    <p>Lorem, ipsum dolor.</p>
-                    <p>Lorem, ipsum dolor.</p>
-                    <p>Lorem, ipsum dolor.</p>
+                    <div>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                    </div>
+                    <div>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                        <a href="#">Lorem, ipsum dolor.</a>
+                    </div>
                 </div>
-                <div>
-                    <p>Lorem, ipsum dolor.</p>
-                    <p>Lorem, ipsum dolor.</p>
-                    <p>Lorem, ipsum dolor.</p>
-                </div>
-                <div>
-                    <p>Lorem, ipsum dolor.</p>
-                    <p>Lorem, ipsum dolor.</p>
-                    <p>Lorem, ipsum dolor.</p>
-                    <p>Lorem, ipsum dolor.</p>
-                </div>
+
             </section>
         </footer>
     )
