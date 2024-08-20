49f7881d005e05bc357faf814ffb57a090ab02a9
Jorge Morales <luifer991@gmail.com>
2024-08-20 17:02:29 -0500

    cart context creation

---
+++ src/context/cart.tsx
@@ -0,0 +1,62 @@
+import React, { createContext, useState } from "react";
+
+// Define the Product interface, including the quantity property
+export interface Product {
+    id: string | number;
+    quantity?: number;
+    // Add other properties of the product object
+}
+
+// 1. create context with proper typing
+interface CartContextType {
+    cart: Product[];
+    addToCart: (product: Product) => void;
+    clearCart: () => void;
+    removeFromCart: (product: Product) => void;
+}
+
+export const CartContext = createContext<CartContextType | undefined>(undefined);
+
+// 2. create provider
+export function CartProvider({ children }: React.PropsWithChildren) {
+    const [cart, setCart] = useState<Product[]>([]);
+
+    const addToCart = (product: Product) => {
+        // check if the product is already in the cart
+        const productInCartIndex = cart.findIndex((item: Product) => item.id === product.id);
+
+        if (productInCartIndex >= 0) {
+            const newCart = structuredClone(cart);
+            if (newCart[productInCartIndex].quantity) {
+                newCart[productInCartIndex].quantity! += 1;
+            } else {
+                newCart[productInCartIndex].quantity = 1;
+            }
+            setCart(newCart);
+        } else {
+            setCart((prevState) => [
+                ...prevState,
+                {
+                    ...product,
+                    quantity: 1,
+                },
+            ]);
+        }
+    };
+
+
+    const removeFromCart = (product: Product) => {
+        setCart((prevState) => prevState.filter((item) => item.id !== product.id));
+    };
+
+    const clearCart = () => {
+        setCart([]);
+    };
+
+    return (
+        <CartContext.Provider 
+        value={{ cart, addToCart, removeFromCart, clearCart }}>
+            {children}
+        </CartContext.Provider>
+    );
+}
