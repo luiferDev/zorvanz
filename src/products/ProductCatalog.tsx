391abdd3e4d874d3afe9507d369ab7ff08a1e5f9
Jorge Morales <luifer991@gmail.com>
2024-08-21 17:31:28 -0500

    cleaning the code use custom hooks

--- src/products/ProductCatalog.tsx
+++ src/products/ProductCatalog.tsx
@@ -3,26 +3,36 @@ import { useFragrances } from "../hooks/useFragrances"
 import Footer from "../UI/Footer"
 import ProductList from "../components/ProductList"
 import CartButton from "../components/CartButton"
+import React from "react"
+import Header from "../components/Header"
+import { useFilters } from "../hooks/useFilters"
+
+// interface FilterProps {
+//     filter: Category | number;
+//     setFilter: React.Dispatch<React.SetStateAction<Category | number>>
+// }
 
 export default function ProductCatalog() {
+
     const { fragrances, loading, error } = useFragrances()
+    const { filterProducts } = useFilters()
 
     return (
         <>
             <NavBar />
+            <aside>
+                <Header />
+            </aside>
             <h1>Productos</h1>
             {loading && <div>Loading...</div>}
             {error && <div>{error}</div>}
             {!loading && !error && fragrances &&
-                fragrances.map(product => {
-                    return (
-                        <>
-                            <ProductList fragrance={product} />
-                            <CartButton
-                                product={product} />
-                        </>
-                    )
-                })}
+                filterProducts(fragrances).map(product => (
+                    <React.Fragment key={product.id}>
+                        <ProductList fragrance={product} />
+                        <CartButton product={product} />
+                    </React.Fragment>
+                ))}
             <Footer />
         </>
     )
