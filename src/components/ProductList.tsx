1580bab39e69612622d422e933564459a89d6b4e
Jorge Morales <luifer991@gmail.com>
2024-08-22 16:19:01 -0500

    adding conditional rendering for stock availability

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -12,31 +12,36 @@ export default function ProductList() {
 
 
     return (
-        <main>
+        <>
             {loading && <div>Loading...</div>}
             {error && <div>{error}</div>}
             {!loading && !error && fragrances &&
                 filteredFragrances.map(product => (
-                    <>
+                    <div className='product__container' key={product.id}>
                         <Link className='link'
-                            to={`/product-catalog/${product.id}`}
-                            key={product.id}>
-                            <article>
-                                <img src={product.imageUrl} alt={product.name} />
-                                <h3>{product.name}</h3>
-                                <p>Descripcion: {product.description}</p>
-                                <p>Categoría: {product.category.categoryName}</p>
-                                <p>Precio: ${product.price}</p>
-                                <p>Stock: {product.stock}</p>
+                            to={`/product-catalog/${product.id}`}>
+                            <article className='product__list'>
+                                <img className='product__img'
+                                    src={product.imageUrl} alt={product.name} />
+                                <div className='info__container'>
+                                    <h3 className='product__title'>{product.name}</h3>
+                                    <p><strong>Descripcion: </strong>{product.description}</p>
+                                    <p><strong>Categoría: </strong>{product.category.categoryName}</p>
+                                    <p><strong>Precio: </strong>${product.price}</p>
+                                    {
+                                        product.stock === 0 && 
+                                        <strong className='no__stock'>Producto Agotado</strong> || 
+                                        <strong className='stock'>Disponible</strong>
+                                    }
+                                </div>
                             </article>
                         </Link>
                         <div className='add__cart__product'>
                             <CartButton product={product} />
                         </div>
-                        
-                    </>
+                    </div>
                 ))
             }
-        </main>
+        </>
     )
 }
