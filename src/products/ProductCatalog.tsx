ed7a1380f8b139974e4860affe45d907f05a22b5
Jorge Morales <luifer991@gmail.com>
2024-08-21 22:52:02 -0500

    refactoring code and affing context for filtering

--- src/products/ProductCatalog.tsx
+++ src/products/ProductCatalog.tsx
@@ -1,39 +1,24 @@
 import { NavBar } from "../components/NavBar"
-import { useFragrances } from "../hooks/useFragrances"
 import Footer from "../UI/Footer"
 import ProductList from "../components/ProductList"
-import CartButton from "../components/CartButton"
-import React from "react"
 import Header from "../components/Header"
-import { useFilters } from "../hooks/useFilters"
-
-// interface FilterProps {
-//     filter: Category | number;
-//     setFilter: React.Dispatch<React.SetStateAction<Category | number>>
-// }
+import { FiltersProvider } from "../context/filterProducts"
 
 export default function ProductCatalog() {
 
-    const { fragrances, loading, error } = useFragrances()
-    const { filterProducts } = useFilters()
-
     return (
-        <>
-            <NavBar />
-            <aside>
-                <Header />
-            </aside>
-            <h1>Productos</h1>
-            {loading && <div>Loading...</div>}
-            {error && <div>{error}</div>}
-            {!loading && !error && fragrances &&
-                filterProducts(fragrances).map(product => (
-                    <React.Fragment key={product.id}>
-                        <ProductList fragrance={product} />
-                        <CartButton product={product} />
-                    </React.Fragment>
-                ))}
-            <Footer />
-        </>
+        <FiltersProvider >
+            <>
+                <NavBar />
+                <aside>
+                    <Header />
+                </aside>
+                <h1>Productos</h1>
+                <main>
+                    <ProductList />
+                </main>
+                <Footer />
+            </>
+        </FiltersProvider>
     )
 }
