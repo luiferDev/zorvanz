d7982940a11ae4abc4dea8cbaddc28061458d891
Jorge Morales <luifer991@gmail.com>
2024-08-21 22:54:33 -0500

    fixing errors and refactoring the code

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,31 +1,45 @@
-import { useId, useState } from "react"
+import React, { useId, useState } from "react"
+import { useFilters } from "../hooks/useFilters"
 
 export default function Filters() {
-
-    const [minPrice, SetMinPrice] = useState('0')
+    const { setFilters } = useFilters()
+    const [minPrice, SetMinPrice] = useState(0)
     const minPriceFilterId = useId()
     const categoryFilterId = useId()
 
-    const handleMinPrice = (event: React.ChangeEvent<HTMLInputElement>) => {
-        SetMinPrice(event.target.value)
+    const handleChangeMinPrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = Number(event.target.value) // Convertir a número
+        SetMinPrice(value)
+        setFilters(prevState => ({
+            ...prevState,
+            price: value
+        }))
     }
 
-    const handleChanceCategory = () => {
-
+    const handleChangeCategory = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setFilters(prevState => ({
+            ...prevState,
+            categoryName: event.target.value 
+        }))
     }
 
     return (
         <section>
             <div>
                 <label htmlFor={minPriceFilterId}>Precio</label>
-                <input type="range" id={minPriceFilterId}
-                    min='0' max='100000' onChange={handleMinPrice} />
+                <input
+                    type="range"
+                    id={minPriceFilterId}
+                    min={0}
+                    max={500}
+                    value={minPrice}
+                    onChange={handleChangeMinPrice}
+                />
                 <span>{minPrice}</span>
             </div>
             <div>
-                <label htmlFor={categoryFilterId}
-                    onChange={handleChanceCategory}>Categoría</label>
-                <select id={categoryFilterId}>
+                <label htmlFor={categoryFilterId}>Categoría</label>
+                <select id={categoryFilterId} onChange={handleChangeCategory}>
                     <option value="all">Todos</option>
                     <option value="VELAS_AROMATICAS">Velas Arómaticas</option>
                     <option value="FRAGANCIAS_CORPORALES">Fragancias Corporales</option>
@@ -35,3 +49,4 @@ export default function Filters() {
         </section>
     )
 }
+
