084f38cb359b0f1a546fb51aa64bb3610c3f6ed5
luifer991 <luifer991@gmail.com>
2024-11-30 10:11:21 -0500

    eliminate useContext for zustand to filter products

--- src/products/ProductCatalog.tsx
+++ src/products/ProductCatalog.tsx
@@ -2,28 +2,25 @@ import { NavBar } from "../components/NavBar"
 import Footer from "../UI/Footer"
 import ProductList from "../components/ProductList"
 import Header from "../components/Header"
-import { FiltersProvider } from "../context/filterProducts"
 import '../styles/product-catalog.css'
-import PP from "../nuevo/PP"
 
 export default function ProductCatalog() {
 
     return (
-        <FiltersProvider >
-            <>
-                <NavBar />
-                <h1 className="title__product__list">Nuestros Productos</h1>
-                <div className="product__catalog">
-                    <aside className="filters__product__list">
-                        <Header />
-                    </aside>
-                    <main className="product__list__container">
-                        <ProductList />
-                    </main>
-                </div>
-                <PP />
-                <Footer />
-            </>
-        </FiltersProvider>
+
+        <>
+            <NavBar />
+            <h1 className="title__product__list">Nuestros Productos</h1>
+            <div className="product__catalog">
+                <aside className="filters__product__list">
+                    <Header />
+                </aside>
+                <main className="product__list__container">
+                    <ProductList />
+                </main>
+            </div>
+            <Footer />
+        </>
+
     )
 }
