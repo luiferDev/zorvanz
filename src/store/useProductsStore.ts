929ee70db7045dca1f818e3405a606a478248bd1
luifer991 <luifer991@gmail.com>
2024-11-30 10:53:06 -0500

    refactor code

--- src/store/useProductsStore.ts
+++ src/store/useProductsStore.ts
@@ -1,29 +1,41 @@
 import { create } from 'zustand'
-import { PaginatedResponse, Category, Product } from '../types/interfaces'
+import { ProductDefinition } from '../types/interfaces'
 
-export interface ProductDefinition {
-    id: number
-    name: string
-    imageUrl: string
-    price: number
-    description: string
-    stock: number
-    popularity: number
-    category: Category
-    selectedProduct?: number
-}
-
+// Estado y acciones del store
 interface State {
-    products: Product[]
-    fetchProducts: () => Promise<void>
+    products: ProductDefinition[]
+    filteredProducts: ProductDefinition[] // Lista de productos filtrados
+    category: ProductDefinition['category']['categoryName']
+    price: number
 }
 
-export const useProductsStore = create<State>((set) => ({
-    products: [],
+interface Action {
+    filterProductsCategory: (category: State['category']) => void
+    filterProductsPrice: (price: State['price']) => void
+    setProducts: (products: ProductDefinition[]) => void // Acción para establecer productos
+}
 
-    fetchProducts: async () => {
-            const res = await fetch("/mocks.json")   
-            const json: PaginatedResponse = await res.json()
-            set({ products: json.content })
-    },
+// Store de Zustand
+export const useProductsStore = create<State & Action>((set) => ({
+    products: [], // Productos originales
+    filteredProducts: [], // Productos filtrados
+    category: '',
+    price: 0,
+
+    // Acción para establecer productos originales
+    setProducts: (products: ProductDefinition[]) => set(() => ({ products, filteredProducts: products })),
+
+    // Filtro por categoría
+    filterProductsCategory: (category: string) =>
+        set((state) => ({
+            category,
+            filteredProducts: state.products.filter((product) => product.category.categoryName === category),
+        })),
+
+    // Filtro por precio máximo
+    filterProductsPrice: (price: number) =>
+        set((state) => ({
+            price,
+            filteredProducts: state.products.filter((product) => product.price <= price),
+        })),
 }))
