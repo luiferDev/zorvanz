a5fac74ede66b49c7b0c876d95fc3d98d136f818
luifer991 <luifer991@gmail.com>
2024-12-06 22:39:32 -0500

    refactoring code

--- src/UI/Products.tsx
+++ src/UI/Products.tsx
@@ -9,6 +9,10 @@ export function Products() {
 
     const { data, isLoading, isError } = useFetchProducts()
 
+    if (isLoading) return <div>Loading...</div>;
+    if (isError) return <div>Error: producto no encontrado</div>;
+    if (!data) return null;
+
     return (
         <>
             <Texts
@@ -16,40 +20,35 @@ export function Products() {
                 subtitle="Productos Destacados"
                 body="Explora nuestra gama de perfumes, velas y productos de cuidado personal. Cada uno hecho a la medida para ti."
             />
-            {isLoading && <div>Loading...</div>}
-            {isError && <div>Error: no se pudo acceder a los datos</div>}
-            {!isLoading && !isError && (
-                <>
-                    <ul className='container__list'>
-                        {data?.map(product => {
-                            return (
-                                //<Link className='link' to={`/product-catalog/${f.id}`} key={f.id}>
-                                <section key={product.id} className='card__container'>
-                                    <motion.img 
-                                    whileHover={{ scale: [null, 1.1], translateY: [0, -5, -4] }}
-                                    transition={{ duration: 0.3 }}
-                                    src={product.imageUrl} alt={product.name} />
-                                    <div className='li'>
-                                        <h3>{product.name}</h3>
-                                        <p>{product.category.categoryName}</p>
-                                    </div>
-                                    <div className='card__footer'>
-                                        <p className='price'>${product.price}</p>
-                                        <CartButton product={product} />
-                                    </div>
-                                </section>
-                                // </Link>
-                            )
-                        })}
-                    </ul>
-                    <motion.div 
-                    whileTap={{ scale: 0.9 }}
-                    transition={{ type: "spring", stiffness: 400, damping: 17 }}
-                    className='container__btn'>
-                        <Link to="/product-catalog" className='more__btn'>ver más</Link>
-                    </motion.div>
-                </>
-            )}
+
+            <ul className='container__list'>
+                {data?.map(product => {
+                    return (
+                        //<Link className='link' to={`/product-catalog/${f.id}`} key={f.id}>
+                        <section key={product.id} className='card__container'>
+                            <motion.img
+                                whileHover={{ scale: [null, 1.1], translateY: [0, -5, -4] }}
+                                transition={{ duration: 0.3 }}
+                                src={product.imageUrl} alt={product.name} />
+                            <div className='li'>
+                                <h3>{product.name}</h3>
+                                <p>{product.category.categoryName}</p>
+                            </div>
+                            <div className='card__footer'>
+                                <p className='price'>${product.price}</p>
+                                <CartButton product={product} />
+                            </div>
+                        </section>
+                        // </Link>
+                    )
+                })}
+            </ul>
+            <motion.div
+                whileTap={{ scale: 0.9 }}
+                transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                className='container__btn'>
+                <Link to="/product-catalog" className='more__btn'>ver más</Link>
+            </motion.div>
         </>
     )
 }
