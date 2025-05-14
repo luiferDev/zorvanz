49a416354ddff2617f314133956bf5bb24c6c17a
luifer991 <luifer991@gmail.com>
2025-05-13 22:09:42 -0500

    fix(Section): update class name for Section component to use Tailwind CSS styles

--- src/UI/Section.tsx
+++ src/UI/Section.tsx
@@ -3,7 +3,7 @@ import "../styles/section.css"
 
 export function Section({ children }: React.PropsWithChildren) {
     return (
-        <div className="section">
+        <div className="bg-[#1c2470] w-full h-[50vh] lg:w-full lg:h-[50vh]">
             {children}
         </div>
     )
