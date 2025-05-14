4d56556d00ce73a8d75dcb4b71cab23b1aabd28e
luifer991 <luifer991@gmail.com>
2025-05-14 12:48:11 -0500

    refactor(ErrorPage): improve layout and styling for better readability

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,29 +1,34 @@
-import { isRouteErrorResponse, Link, useRouteError } from "react-router-dom";
-import './styles/styles.css'
+import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom'
 
 export default function ErrorPage() {
-    const error = useRouteError();
+    const error = useRouteError()
 
     if (isRouteErrorResponse(error)) {
-        console.error(error);
+        console.error(error)
 
         return (
-            <div id="error-page">
-                <h1><Link to="/">Oops!</Link></h1>
-                <p>Sorry, an unexpected error has occurred.</p>
-                <p>
+            <div className="flex flex-col items-center w-full relative top-80">
+                <h1 className="lg:text-7xl pb-4">
+                    <Link to="/">Oops!</Link>
+                </h1>
+                <p className="text-2xl">
+                    Sorry, an unexpected error has occurred.
+                </p>
+                <p className="text-2xl text-red-400">
                     <i>{error.statusText || error.data?.message}</i>
-                </p>        
+                </p>
             </div>
-        );
+        )
     } else {
-        console.error("An unknown error occurred");
+        console.error('An unknown error occurred')
 
         return (
-            <div id="error-page">
-                <h1>Oops!</h1>
-                <p>Sorry, an unexpected error has occurred.</p>
+            <div className="flex flex-col items-center w-full relative top-80">
+                <h1 className="lg:text-7xl pb-4">Oops!</h1>
+                <p className="text-2xl">
+                    Sorry, an unexpected error has occurred.
+                </p>
             </div>
-        );
+        )
     }
 }
