8ce5b7e32b110977b26072fd72b54eea0c8d1b82
luifer991 <luifer991@gmail.com>
2025-05-13 22:11:49 -0500

    refactor(ContactsFrom): update layout and styling to use Tailwind CSS classes

--- src/components/ContactsFrom.tsx
+++ src/components/ContactsFrom.tsx
@@ -1,16 +1,23 @@
-import '../styles/Zorvanz.css'
-
 export default function Zorvanz() {
     return (
-        <>
-            <section className='zorvanz__info'>
-                <h2>Conoce Zorvanz</h2>
-                <p>Zorvanz es una marca dedicada a crear experiencias olfativas únicas. Con una amplia gama de productos que incluyen perfumes personalizados, velas aromáticas y todo lo relacionado con los aromas, cada creación es un reflejo de la calidad y la pasión por la excelencia.
-                </p>
-                <p>
-                    Nos enfocamos en personalizar cada fragancia, permitiendo a nuestros clientes expresar su identidad a través de aromas únicos. En Zorvanz, creemos que cada persona es única y merece una fragancia que lo sea también. ¡Descubre la esencia que te define!
-                </p>
-            </section>
-        </>
+        <section className="m-0 lg:pl-24">
+            <h2 className="mt-20 mb-4 ml-12 text-[40px] lg:text-[64px]">
+                Conoce Zorvanz
+            </h2>
+            <p className="mt-8 mb-4 ml-12 w-[20ch] lg:w-[45ch] lg:text-[24px]">
+                Zorvanz es una marca dedicada a crear experiencias olfativas
+                únicas. Con una amplia gama de productos que incluyen perfumes
+                personalizados, velas aromáticas y todo lo relacionado con los
+                aromas, cada creación es un reflejo de la calidad y la pasión
+                por la excelencia.
+            </p>
+            <p className="mt-8 mb-4 ml-12 w-[20ch] lg:w-[45ch] lg:text-[24px]">
+                Nos enfocamos en personalizar cada fragancia, permitiendo a
+                nuestros clientes expresar su identidad a través de aromas
+                únicos. En Zorvanz, creemos que cada persona es única y merece
+                una fragancia que lo sea también. ¡Descubre la esencia que te
+                define!
+            </p>
+        </section>
     )
 }
