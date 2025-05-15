684ab48874f718a9c7f091c448b5a0a11ed9f049
luifer991 <luifer991@gmail.com>
2025-05-14 23:03:43 -0500

    refactor(LoginPage): enhance styling and remove unused CSS for cleaner code

--- src/Auth/LoginPage.tsx
+++ src/Auth/LoginPage.tsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
         <>
             <BackButton />
             <form
-                className="form"
+                className="w-full bg-white block max-w-[350px] shadow-[0_10px_15px_-3px_rgba(0,0,0,0.1),0_4px_6px_-2px_rgba(0,0,0,0.05)] mx-auto my-[10%] p-4 rounded-lg"
                 action={url}
                 onSubmit={handleSubmit(async (data) => {
                     const res = await loginRequest(data.userName, data.password)
@@ -23,9 +23,12 @@ export default function LoginPage() {
                     console.log(res)
                 })}
             >
-                <p className="form-title">Iniciar Sesión</p>
-                <div className="input-container">
+                <p className="text-xl leading-7 font-semibold text-center text-black">
+                    Iniciar Sesión
+                </p>
+                <div className="relative">
                     <input
+                        className="bg-white text-sm leading-5 w-[300px] shadow-[0_1px_2px_0_rgba(0,0,0,0.05)] pr-12 p-4 rounded-lg outline-none border mx-0 my-2 border-solid"
                         type="text"
                         placeholder="usuario123"
                         {...register('userName', { required: true })}
@@ -34,18 +37,27 @@ export default function LoginPage() {
                 </div>
                 <div className="input-container">
                     <input
+                        className="bg-white text-sm leading-5 w-[300px] shadow-[0_1px_2px_0_rgba(0,0,0,0.05)] pr-12 p-4 rounded-lg outline-none border mx-0 my-2 border-solid"
                         type="password"
                         placeholder="**********"
                         {...register('password', { required: true })}
                     />
                 </div>
 
-                <button className="submit" type="submit">
+                <button
+                    className="block bg-[#1c2470]
+					text-white text-sm leading-5 font-medium 
+					w-full uppercase px-5 py-3 rounded-lg outline-none border mx-0 my-2 border-solid"
+                    type="submit"
+                >
                     Login
                 </button>
-                <p className="signup-link">
+                <p className="text-[#6B7280] text-sm leading-5 text-center">
                     ¿No tienes una cuenta?
-                    <Link to="/register"> Registrate</Link>
+                    <Link className="underline" to="/register">
+                        {' '}
+                        Registrate
+                    </Link>
                 </p>
             </form>
         </>
