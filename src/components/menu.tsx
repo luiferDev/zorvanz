46ffaf2d6c735230d6d25deb791138e2563eb263
luifer991 <luifer991@gmail.com>
2025-05-14 19:23:17 -0500

    refactor(Menu): enhance layout and styling, remove unused CSS for cleaner code

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -39,11 +39,14 @@ function Menu() {
                 initial={{ opacity: 0, x: -100 }}
                 animate={{ opacity: isOpen ? 1 : 0, x: isOpen ? 0 : -100 }}
                 transition={{ duration: 0.3 }}
-                className={`menu-options ${isOpen ? 'open' : ''}`}
+                className={`absolute top-0 left-0 w-[80vw] h-[100vh] bg-[#701C1C] px-5 py-20 z-[2] lg:bg-transparent lg:top-[-58px] lg:left-[10px] lg:w-[40vw] lg:h-[10vh] lg:z-[2] lg:opacity-100! lg:transform! lg:translate-x-0! ${
+                    isOpen ? 'open' : ''
+                }`}
             >
-                <ul className="navbar__links">
+                <ul className="list-none m-0 p-0 flex flex-col gap-8 lg:flex-row lg:opacity-100! lg:pb-16 lg:pl-16 lg:transform! lg:translate-x-0! lg:animate-none z-50">
                     {menuItems.map((item) => (
                         <motion.li
+                            className="my-2.5 text-white! lg:opacity-100! lg:transform! lg:translate-x-0! lg:text-black!"
                             key={item.path}
                             initial={{ x: -100, opacity: 0 }}
                             animate={{
@@ -52,7 +55,19 @@ function Menu() {
                             }}
                             transition={{ delay: item.delay, type: 'just' }}
                         >
-                            <NavLink to={item.path}>{item.label}</NavLink>
+                            <NavLink
+                                to={item.path}
+                                className={({ isActive }) =>
+                                    // clases base (desktop y mobile) + las de "activo" cuando isActive === true
+                                    `font-normal lg:text-2xl lg:text-black ${
+                                        isActive
+                                            ? 'lg:border-4 lg:border-[#701C1C] lg:p-2.5 lg:rounded-3xl lg:text-black'
+                                            : ''
+                                    }`
+                                }
+                            >
+                                {item.label}
+                            </NavLink>
                         </motion.li>
                     ))}
                 </ul>
