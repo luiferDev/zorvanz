751fd43e3cf3df34e0556a371586cd3cae7fe7b2
luifer991 <luifer991@gmail.com>
2025-05-14 23:38:36 -0500

    refactor(ImagesSection): simplify layout and remove unused CSS for cleaner code

--- src/components/ImagesSection.tsx
+++ src/components/ImagesSection.tsx
@@ -1,16 +1,15 @@
 import fragrance1 from '/contact_fragrance1.svg'
 import fragrance2 from '/fragrance-blue.svg'
 import fragrance3 from '/fragrance-red.svg'
-import '../styles/imagesection.css'
 
 export default function ImagesSection() {
     return (
         <>
-            <section className='image__section'>
-                <img id='img' src={fragrance2} alt="fragrance blue" />
-                <div className='img__section2'>
-                    <img src={fragrance1} alt="fragrance red" />
-                    <img src={fragrance3} alt="fragrance red with flowers" />
+            <section className='flex flex-col justify-center items-center mt-12 mb-12 w-full lg:pt-16 lg:pr-40'>
+                <img className='w-96 h-auto aspect-video rounded-bl-3xl' src={fragrance2} alt="fragrance blue" />
+                <div className='pt-2.5'>
+                    <img className='w-36 h-auto aspect-square pr-3' src={fragrance1} alt="fragrance red" />
+                    <img className='w-36 h-auto aspect-square' src={fragrance3} alt="fragrance red with flowers" />
                 </div>
             </section>
         </>
