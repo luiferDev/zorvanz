738aeca4fe7867b9165091a55de9deb9bdef1412
luifer991 <luifer991@gmail.com>
2025-05-16 20:33:12 -0500

    fix(Hero): enhance layout and styling for better presentation

--- src/about/UI/Hero.tsx
+++ src/about/UI/Hero.tsx
@@ -1,14 +1,14 @@
 
 export default function Hero() {
     return (
-        <section id="hero">
-            <header>
-                <h1>Nuestra Historia</h1>
-                <article>
-                    <p>Zorvanz es más que una marca de fragancias; es una invitación a descubrir tu esencia única. Desde nuestros inicios, nos hemos dedicado a crear experiencias olfativas personalizadas que reflejan la individualidad de cada persona.</p>
-                </article>
+        <section className="flex flex-col justify-center items-center lg:flex-row lg:h-screen">
+            <header className="w-full pt-20 px-8 flex flex-col justify-center items-center m-8 gap-4 lg:w-1/2 lg:p-32">
+                <h1 className="pb-4 text-6xl lg:ml-[-3rem]">Nuestra Historia</h1>
+                <p className="text-xl">
+                    Zorvanz es más que una marca de fragancias; es una invitación a descubrir tu esencia única. Desde nuestros inicios, nos hemos dedicado a crear experiencias olfativas personalizadas que reflejan la individualidad de cada persona.
+                </p>
             </header>
-            <img src="/zorvanzAbout.jpg" alt="imagen de una mujer con unos perfumes" />
+            <img className="aspect-video w-full h-3/5 p-8 lg:w-1/2" src="/zorvanzAbout.jpg" alt="imagen de una mujer con unos perfumes" />
         </section>
     )
 }
