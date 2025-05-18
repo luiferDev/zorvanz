46ce152edcfe559f0efc65dec1737ef4a0f38987
luifer991 <luifer991@gmail.com>
2025-05-17 19:35:49 -0500

    fix(Testimonials): refactor component to use dynamic testimonials and improve layout

--- src/about/UI/Testimonials.tsx
+++ src/about/UI/Testimonials.tsx
@@ -1,25 +1,27 @@
 import Slider from 'react-slick'
 import { settings2 } from '../../components/settings/sliderSettings'
+import { testimonials } from '../../hooks/constants'
 
 export default function Testimonials() {
     return (
-        <section id="testimonials">
-            <header>
-                <h2>Testimonios</h2>
+        <section className="flex flex-col justify-center gap-12 m-12">
+            <header className="w-full text-center gap-4">
+                <h2 className="text-5xl">Testimonios</h2>
             </header>
             <Slider {...settings2}>
-                <blockquote>
-                    <p>"Zorvanz no es solo un perfume, es una experiencia olfativa única que me acompaña todos los días."  Cliente Satisfecho</p>
-                </blockquote>
-                <blockquote>
-                    <p>"Zorvanz no es solo un perfume, es una experiencia olfativa única que me acompaña todos los días."  Cliente Satisfecho</p>
-                </blockquote>
-                <blockquote>
-                    <p>"Zorvanz no es solo un perfume, es una experiencia olfativa única que me acompaña todos los días."  Cliente Satisfecho</p>
-                </blockquote>
-                <blockquote>
-                    <p>"Zorvanz no es solo un perfume, es una experiencia olfativa única que me acompaña todos los días."  Cliente Satisfecho</p>
-                </blockquote>
+                {testimonials.map((testimonial) => (
+                    <blockquote
+                        key={testimonial.id}
+                        className="h-[45vh] lg:h-[30vh] flex flex-col justify-center items-center text-center"
+                    >
+                        <p className="text-2xl text-center my-8 mx-4 lg:text-4xl">
+                            {testimonial.text}
+                        </p>
+                        <strong className="text-2xl text-center m-8">
+                            {testimonial.author}
+                        </strong>
+                    </blockquote>
+                ))}
             </Slider>
         </section>
     )
