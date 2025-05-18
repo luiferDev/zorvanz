3d0e73bf9842784495d0db6af4e9ded717d7ec92
luifer991 <luifer991@gmail.com>
2025-05-17 19:07:45 -0500

    fix(Values): refactor component to use dynamic values and improve layout

--- src/about/UI/Values.tsx
+++ src/about/UI/Values.tsx
@@ -1,38 +1,21 @@
+import { values } from '../../hooks/constants'
 
 export default function Values() {
     return (
-        <section id="values">
+        <section className='h-[1380px] lg:h-screen flex flex-col justify-center items-center gap-12 bg-zorvanz-blue'>
             <header>
-                <h2>Nuestros Valores</h2>
+                <h2 className='text-5xl pl-4 pb-4 text-zorvanz-beige'>Nuestros Valores</h2>
             </header>
-            <article>
-
-                <div>
-                    <img src="/amorIcon.png" alt="una mano con un corazon" />
-                    <h2>Amor</h2>
-                    <p>Transmitimos el amor con el que creamos nuestros productos, enfocándonos en satisfacer al cliente y en el progreso de nuestra empresa, con la satisfacción del cliente como objetivo principal.</p>
-                </div>
-
-                <div>
-                    <img src="/honestyIcon.png" alt="una mano tocando un corazon" />
-                    <h2>Honestidad</h2>
-                    <p>Zorvanz venderá productos basados en la honestidad, reflejando lo que promete y priorizando la integridad y el respeto, fundamentales para la reputación de la empresa.</p>
-                </div>
-
-                <div>
-                    <img src="/innovationIcon.png" alt="una cabeza con un bombillo en el medio" />
-                    <h2>Innovación</h2>
-                    <p>Zorvanz se distingue por su innovación, creando fragancias personalizadas que promueven la comodidad del cliente, diferenciándose de las compañías comunes de perfumes.</p>
-                </div>
-
-                <div>
-                    <img src="/optimismIcon.png" alt="una cabeza con un sol en la nuca" />
-                    <h2>Optimismo</h2>
-                    <p>Zorvanz se caracterizará por su optimismo, resiliencia y fortaleza, manteniendo una perspectiva positiva y aprendiendo de cada experiencia para asegurar el desarrollo continuo de la empresa.</p>
-                </div>
-
+            <article className='flex flex-col gap-4 lg:justify-center lg:items-center lg:w-full lg:flex-row'>
+                {values.map((value) => (
+					<div className='w-[35ch] text-zorvanz-beige p-4 rounded-3xl lg:h-80
+					border-[1px] border-solid border-bl-[rgba(255_255_255_0.1)] bg-gradient-to-b from-[#fff2] to-transparent shadow-xl shadow-black backdrop-blur-md' key={value.id}>
+                        <img src={value.icon} alt={value.alt} />
+                        <h2 className='text-2xl pb-4'>{value.title}</h2>
+                        <p className='text-base'>{value.description}</p>
+                    </div>
+                ))}
             </article>
-
         </section>
     )
 }
