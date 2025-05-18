4fbbeb92ded59fbdde6a7124c830d18f169729ae
luifer991 <luifer991@gmail.com>
2025-05-17 19:07:54 -0500

    fix(Vision): enhance layout and styling for improved presentation

--- src/about/UI/Vision.tsx
+++ src/about/UI/Vision.tsx
@@ -1,14 +1,23 @@
-
 export default function Vision() {
     return (
-        <section id="vision">
-
-            <header>
-                <h2>Visión</h2>
-                <p>Queremos ser un líder global en fragancias y perfumes, exportando sus productos al extranjero. A futuro, desea mantenerse fiel a sus valores de innovación, integridad, transparencia, honestidad, enfoque en el cliente y calidad. La visión es ser reconocida internacionalmente por priorizar el bienestar, la seguridad, la comodidad y la satisfacción de sus clientes.</p>
+        <section className="flex flex-col justify-center my-12 items-center lg:flex-row">
+            <header className="lg:w-1/2 lg:p-8 flex flex-col lg:ml-12 text-start gap-4 lg:m-4">
+                <h2 className="text-5xl text-start!">Visión</h2>
+                <p className="text-xl w-[25ch] lg:w-[50ch]">
+                    Queremos ser un líder global en fragancias y perfumes,
+                    exportando sus productos al extranjero. A futuro, desea
+                    mantenerse fiel a sus valores de innovación, integridad,
+                    transparencia, honestidad, enfoque en el cliente y calidad.
+                    La visión es ser reconocida internacionalmente por priorizar
+                    el bienestar, la seguridad, la comodidad y la satisfacción
+                    de sus clientes.
+                </p>
             </header>
 
-            <img src="/mujerEnELCampo.jpg" alt="imagen de una mujer en un campo" />
+            <img className="aspect-video w-full p-8 lg:w-1/2"
+                src="/mujerEnELCampo.jpg"
+                alt="imagen de una mujer en un campo"
+            />
         </section>
     )
 }
