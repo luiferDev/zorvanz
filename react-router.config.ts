7e200df9588dfe870d3a96c6d9d96e647969b8cb
luifer991 <luifer991@gmail.com>
2025-05-29 11:14:58 -0500

    refactor: remove unused prerender configuration from react-router setup

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,14 +3,4 @@ import type { Config } from '@react-router/dev/config'
 export default {
     appDirectory: 'src',
     ssr: false,
-    async prerender() {
-        return [
-            '/',
-            '/about',
-            '/product-catalog',
-            '/login',
-            '/cart',
-            '/add-product',
-        ]
-    },
 } satisfies Config
