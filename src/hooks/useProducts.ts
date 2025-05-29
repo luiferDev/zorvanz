2aae3023d213f354c5691745963739a501b2cfbf
luifer991 <luifer991@gmail.com>
2025-05-29 14:06:54 -0500

    fix: update fetchProducts function to use correct API endpoint and response structure

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,10 +1,12 @@
-import api from "../api/zorvanz-api"
-import { useQuery } from "@tanstack/react-query"
-import { PaginatedResponse } from "../types/interfaces"
+import api from '../api/zorvanz-api'
+import { useQuery } from '@tanstack/react-query'
+import { PaginatedResponse } from '../types/interfaces'
 
 async function fetchProducts() {
-    const { data } = await api.get<PaginatedResponse>('/mocks.json')
-    return data.content
+    const { data } = await api.get<PaginatedResponse>(
+        'http://localhost:5297/Product?pageNumber=1&pageSize=3',
+    )
+    return data.data
 }
 
 export function useFetchProducts() {
