6c9a2b7f47734b5c103aa50fea518a184ce9e916
luifer991 <luifer991@gmail.com>
2025-05-29 21:18:03 -0500

    fix: refactor auth store to improve structure and add missing properties

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,20 +1,38 @@
-import { create } from "zustand"
-import { persist } from "zustand/middleware"
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface State {
     token: string
+    profile: Profile | null
+    isAuth: boolean
+}
+
+interface Profile {
+    id: string
+	name: string
+	lastName: string
+    email: string
+    username: string
 }
 
 interface Actions {
     setToken: (token: string) => void
+    setProfile: ( profile: Profile ) => void
+    logout: () => void
 }
 
-export const useAuthStore = create<State & Actions>()(persist(
-    (set) => ({
-        token: "",
-        setToken: (token: string) => set({ token })
-    }),
-    {
-        name: "auth"
-    }
-))
+export const useAuthStore = create<State & Actions>()(
+    persist(
+        (set) => ({
+			token: '',
+			profile: null,
+            isAuth: false,
+			setToken: (token: string) => set({ token, isAuth: true }),
+			setProfile: (profile: Profile) => set({ profile }),
+            logout: () => set({ token: '', isAuth: false }),
+        }),
+        {
+            name: 'auth',
+        },
+    ),
+)
