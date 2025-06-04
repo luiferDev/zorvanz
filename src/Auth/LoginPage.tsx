8f2afebd9d5f09eb203415b6c531dabca99d72c7
luifer991 <luifer991@gmail.com>
2025-06-03 21:29:17 -0500

    feat: enhance LoginPage with profile fetching and navigation for Admin role

--- src/Auth/LoginPage.tsx
+++ src/Auth/LoginPage.tsx
@@ -1,30 +1,34 @@
 import { useForm } from 'react-hook-form'
-import { loginRequest } from '../api/loginRequest'
+import { loginRequest, profileRequest } from '../api/loginRequest'
 import { useAuthStore } from '../store/auth'
 import { Link } from 'react-router'
 import BackButton from '../UI/back-button'
 import { NavBar } from '../components/NavBar'
+import { useNavigate } from 'react-router'
 
 export default function LoginPage() {
     const { register, handleSubmit } = useForm()
-	const setToken = useAuthStore((state) => state.setToken)
-	
-	console.log('Zustand token:', useAuthStore.getState().token)
-	console.log('localStorage auth:', localStorage.getItem('auth'))
-	//TODO: create a profile store and set the profile after login
-	// TODO: handle errors from the login request
-	// TODO: Navigate to the home page wheteher it's an user or a dashboard it's an admin
+    const setToken = useAuthStore((state) => state.setToken)
+    const setProfile = useAuthStore((state) => state.setProfile)
+    const navigate = useNavigate()
     return (
-		<>
-			<NavBar />
+        <>
+            <NavBar />
             <BackButton />
             <form
                 className="w-full bg-white block max-w-[350px] shadow-[0_10px_15px_-3px_rgba(0,0,0,0.1),0_4px_6px_-2px_rgba(0,0,0,0.05)] mx-auto my-[10%] p-4 rounded-lg"
-            
                 onSubmit={handleSubmit(async (data) => {
                     const res = await loginRequest(data.userName, data.password)
+                    const resProfile = await profileRequest({
+                        username: data.userName,
+                    })
                     setToken(res.token)
-                    console.log(res.token)
+                    setProfile(resProfile.data)
+
+                    if (resProfile.data.role === 'Admin') {
+                        return navigate('/dashboard')
+                    }
+                    navigate('/profile')
                 })}
             >
                 <p className="text-xl leading-7 font-semibold text-center text-black">
@@ -51,7 +55,7 @@ export default function LoginPage() {
                 <button
                     className="block bg-[#1c2470]
 					text-white text-sm leading-5 font-medium 
-					w-full uppercase px-5 py-3 rounded-lg outline-none border mx-0 my-2 border-solid"
+					w-full uppercase px-5 py-3 rounded-lg outline-none border mx-0 my-2 border-solid cursor-pointer"
                     type="submit"
                 >
                     Login
