9cfa57e277afd9b964c106f2917b70f87c826f9d
luifer991 <luifer991@gmail.com>
2025-06-04 11:21:54 -0500

    feat: add Logo component for site branding and navigation

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router";
+
+export default function Logo() {
+    return (
+        <Link to="/">
+            <img className="logo" src="/Zorvanz_1.png" alt="zorvanz logo" />
+        </Link>
+    )
+}
