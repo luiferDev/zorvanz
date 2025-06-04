645ab78cac1220a1f915b8d07140a4ab947d7ba0
luifer991 <luifer991@gmail.com>
2025-06-04 11:21:04 -0500

    feat: enhance Dashboard component with profile display and improved layout

--- src/Auth/Dashboard.tsx
+++ src/Auth/Dashboard.tsx
@@ -1,22 +1,34 @@
 import { useAuthStore } from '../store/auth'
 import { useNavigate } from 'react-router'
 import { useCallback } from 'react'
+import { NavBar } from '../components/NavBar'
 
 export default function Dashboard() {
+	
+    const logout = useAuthStore((state) => state.logout)
+    const profile = useAuthStore((state) => state.profile)
+    const navigate = useNavigate()
 
-	const logout = useAuthStore((state) => state.logout)
-	const navigate = useNavigate()
+    const handleLogout = useCallback(async () => {
+        await logout()
+        navigate('/')
+    }, [logout, navigate])
 
-	const handleLogout = useCallback(async () => {
-		await logout()
-		navigate('/')
-	}, [logout, navigate])
-
-	return (
-		<div>
-			<h1>Dashboard</h1>
-			<p>Welcome to your dashboard!</p>
-			<button onClick={handleLogout}>Logout</button>
-		</div>
-	)
+    return (
+		<div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+			<NavBar />
+            <h1>Dashboard</h1>
+            {profile && (
+                <div>
+                    <p>Bienvenido: {profile.userName}</p>
+                </div>
+            )}
+            <button
+                className="cursor-pointer px-8 py-4 bg-zorvanz-red text-white rounded-2xl"
+                onClick={handleLogout}
+            >
+                Logout
+            </button>
+        </div>
+    )
 }
