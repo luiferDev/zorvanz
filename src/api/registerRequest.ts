a035a810bd92e5936646631272b0af3cb4635eeb
luifer991 <luifer991@gmail.com>
2025-06-05 22:10:07 -0500

    feat: implement user and admin registration requests

---
+++ src/api/registerRequest.ts
@@ -0,0 +1,38 @@
+import axios from './zorvanz-api'
+
+export const registerUserRequest = async (
+    name: string,
+    lastName: string,
+    userName: string,
+    password: string,
+    email: string,
+) => {
+    const res = await axios.post('/api/register', {
+        name,
+        lastName,
+        userName,
+        password,
+        email,
+        role: 'User',
+    })
+    return res.data
+}
+
+export const registerAdminRequest = async (
+    name: string,
+    lastName: string,
+    userName: string,
+    password: string,
+    email: string,
+) => {
+    const res = await axios.post('/api/Register/admin', {
+        name,
+        lastName,
+        userName,
+        password,
+        email,
+        role: 'Admin',
+    })
+    return res.data
+}
+
