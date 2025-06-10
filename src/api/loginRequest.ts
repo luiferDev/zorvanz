7a7b1898051c26b554b2e2e2bcfbdc04bb070096
luifer991 <luifer991@gmail.com>
2025-06-09 22:06:01 -0500

    fix: update loginRequest API endpoint to use relative path

--- src/api/loginRequest.ts
+++ src/api/loginRequest.ts
@@ -1,7 +1,7 @@
 import axios from "../api/auth";
 
 export const loginRequest = async (userName: string, password: string) => {
-    const res = await axios.post('http://localhost:8080/api/auth/login', {
+    const res = await axios.post('/api/Login', {
         userName,
         password
     })
