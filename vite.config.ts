783ace91728e4acd8cb51a7c2bc0cb194f870c4a
luifer991 <luifer991@gmail.com>
2025-06-11 23:26:04 -0500

    feat: add test configuration for Vitest in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
-import { reactRouter } from "@react-router/dev/vite"
+import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [tailwindcss(), reactRouter()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+        css: true,
+    },
 })
-
