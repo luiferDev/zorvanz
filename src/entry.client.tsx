c8591cb4d19fed5c5265da59d2dd17951024b47b
luifer991 <luifer991@gmail.com>
2025-08-26 19:07:34 -0500

    feat: add Cypress global configuration and cleanup for testing environment

--- src/entry.client.tsx
+++ src/entry.client.tsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client'
 import { HydratedRouter } from 'react-router/dom'
 import './index.css'
 
+declare global {
+    interface Window {
+        Cypress?: any;
+    }
+}
+
+if (window.Cypress) {
+    const root = document.getElementById('root')
+    if (root?.hasAttribute('data-reactroot')) {
+        root.innerHTML = ''
+    }
+}
+
+
 ReactDOM.hydrateRoot(
     document,
     <React.StrictMode>
