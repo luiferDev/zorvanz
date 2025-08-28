ab51c3f4154465017f6a46181a607d39576562b3
luifer991 <luifer991@gmail.com>
2025-08-27 20:25:50 -0500

    fix: improve text clarity and consistency in Zorvanz component

--- src/components/ContactsFrom.tsx
+++ src/components/ContactsFrom.tsx
@@ -6,17 +6,15 @@ export default function Zorvanz() {
             </h2>
             <p className="mt-8 mb-4 ml-12 w-[20ch] lg:w-[45ch] lg:text-[24px]">
                 Zorvanz es una marca dedicada a crear experiencias olfativas
-                únicas. Con una amplia gama de productos que incluyen perfumes
-                personalizados, velas aromáticas y todo lo relacionado con los
-                aromas, cada creación es un reflejo de la calidad y la pasión
-                por la excelencia.
+				únicas. Con una amplia gama de productos que incluyen ambientadores y
+				velas aromáticas y todo lo relacionado cada creación es un
+				reflejo de la calidad y la pasión por la excelencia.
             </p>
             <p className="mt-8 mb-4 ml-12 w-[20ch] lg:w-[45ch] lg:text-[24px]">
                 Nos enfocamos en personalizar cada fragancia, permitiendo a
-                nuestros clientes expresar su identidad a través de aromas
-                únicos. En Zorvanz, creemos que cada persona es única y merece
-                una fragancia que lo sea también. ¡Descubre la esencia que te
-                define!
+				nuestros clientes expresar su identidad y agregarle personalidad a su entorno
+				a través de aromas únicos. En Zorvanz, creemos que si tú eres único, tu hogar y tu entorno también
+				¡Descubre la esencia que te define!
             </p>
         </section>
     )
