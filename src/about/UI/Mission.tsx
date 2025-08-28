71241b2ab985f015d049b4135d19f973eff13668
luifer991 <luifer991@gmail.com>
2025-08-27 22:38:18 -0500

    fix: enhance clarity and consistency in Mission and Vision components

--- src/about/UI/Mission.tsx
+++ src/about/UI/Mission.tsx
@@ -6,7 +6,7 @@ export default function Mission() {
                 <img className="aspect-video w-full p-4 mt-8 lg:w-1/2 lg:mt-0" src="/mission.jpg" alt="imagen de una fragancia" />
                 <header className="m-20 flex flex-col justify-center items-center text-start gap-4">
                     <h2 className="pb-4 text-5xl w-full">Misión</h2>
-                    <p className="text-xl w-[20ch] lg:w-[40ch]">Queremos ser un líder en la industria de las fragancias, ofreciendo productos naturales y respetuosos con el medio ambiente y la piel. Se enfocan en crear aromas que no solo agraden a los sentidos, sino que también promuevan el bienestar del planeta y de los usuarios, buscando inspirar amor y lealtad en cada experiencia con sus fragancias.</p>
+                    <p className="text-xl w-[20ch] lg:w-[40ch]">Queremos ser un líder en la industria de las fragancias, ofreciendo productos que se enfocan en darle bienestar a los usuarios. Se enfocan en crear aromas que no solo agraden a los sentidos, sino que también promuevan el bienestar de los usuarios, buscando inspirar amor y lealtad en cada experiencia con sus fragancias.</p>
                 </header>
             </div>
 
--- src/about/UI/Vision.tsx
+++ src/about/UI/Vision.tsx
@@ -4,7 +4,7 @@ export default function Vision() {
             <header className="lg:w-1/2 lg:p-8 flex flex-col lg:ml-12 text-start gap-4 lg:m-4">
                 <h2 className="text-5xl text-start!">Visión</h2>
                 <p className="text-xl w-[25ch] lg:w-[50ch]">
-                    Queremos ser un líder global en fragancias y perfumes,
+                    Queremos ser un líder global en fragancias,
                     exportando sus productos al extranjero. A futuro, desea
                     mantenerse fiel a sus valores de innovación, integridad,
                     transparencia, honestidad, enfoque en el cliente y calidad.
