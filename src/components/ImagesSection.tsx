87a5e25a9202a1bb198a021c4fc70c563ee7e8f4
luifer991 <luifer991@gmail.com>
2025-08-27 20:25:58 -0500

    fix: simplify ImagesSection component by removing unused images and adjusting layout

--- src/components/ImagesSection.tsx
+++ src/components/ImagesSection.tsx
@@ -1,16 +1,14 @@
 import fragrance1 from '/contact_fragrance1.svg'
-import fragrance2 from '/fragrance-blue.svg'
-import fragrance3 from '/fragrance-red.svg'
 
 export default function ImagesSection() {
     return (
         <>
-            <section className='flex flex-col justify-center items-center mt-12 mb-12 w-full lg:pt-16 lg:pr-40'>
-                <img className='w-96 h-auto aspect-video rounded-bl-3xl' src={fragrance2} alt="fragrance blue" />
-                <div className='pt-2.5'>
-                    <img className='w-36 h-auto aspect-square pr-3' src={fragrance1} alt="fragrance red" />
-                    <img className='w-36 h-auto aspect-square' src={fragrance3} alt="fragrance red with flowers" />
-                </div>
+            <section className="flex flex-col justify-center items-center mt-12 mb-12 w-full lg:pt-16 lg:pr-40">
+                <img
+                    className="w-96 h-auto aspect-square"
+                    src={fragrance1}
+                    alt="fragrance red"
+                />
             </section>
         </>
     )
