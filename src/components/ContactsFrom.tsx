799ccc83ef43866b0a0b0430b946d1d8232cf3dd
luifer991 <luifer991@gmail.com>
2025-09-09 12:48:05 -0500

    fix: update font families for improved consistency in ContactsFrom and CSS styles

--- src/components/ContactsFrom.tsx
+++ src/components/ContactsFrom.tsx
@@ -1,20 +1,22 @@
 export default function Zorvanz() {
     return (
         <section className="m-0 lg:pl-24">
-            <h2 className="mt-20 mb-4 ml-12 text-[40px] lg:text-[64px]">
-                Conoce Zorvanz
+            <h2 className="mt-20 mb-4 ml-12 text-[40px] lg:text-[64px] font-voga">
+                Conoce <span className="font-vanio" >ZORVANZ</span>
             </h2>
             <p className="mt-8 mb-4 ml-12 w-[20ch] lg:w-[45ch] lg:text-[24px]">
                 Zorvanz es una marca dedicada a crear experiencias olfativas
-				únicas. Con una amplia gama de productos que incluyen ambientadores y
-				velas aromáticas y todo lo relacionado cada creación es un
-				reflejo de la calidad y la pasión por la excelencia.
+                únicas. Con una amplia gama de productos que incluyen
+                ambientadores y velas aromáticas y todo lo relacionado cada
+                creación es un reflejo de la calidad y la pasión por la
+                excelencia.
             </p>
             <p className="mt-8 mb-4 ml-12 w-[20ch] lg:w-[45ch] lg:text-[24px]">
                 Nos enfocamos en personalizar cada fragancia, permitiendo a
-				nuestros clientes expresar su identidad y agregarle personalidad a su entorno
-				a través de aromas únicos. En Zorvanz, creemos que si tú eres único, tu hogar y tu entorno también
-				¡Descubre la esencia que te define!
+                nuestros clientes expresar su identidad y agregarle personalidad
+                a su entorno a través de aromas únicos. En Zorvanz, creemos que
+                si tú eres único, tu hogar y tu entorno también ¡Descubre la
+                esencia que te define!
             </p>
         </section>
     )
