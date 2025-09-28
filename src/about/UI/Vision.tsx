600000dde3beec55da784171b73dac365e0d23b8
luifer991 <luifer991@gmail.com>
2025-09-28 12:46:28 -0500

    change vision description

--- src/about/UI/Vision.tsx
+++ src/about/UI/Vision.tsx
@@ -4,17 +4,18 @@ export default function Vision() {
             <header className="lg:w-1/2 lg:p-8 flex flex-col lg:ml-12 text-start gap-4 lg:m-4">
                 <h2 className="text-5xl text-start!">Visión</h2>
                 <p className="text-xl w-[25ch] lg:w-[50ch]">
-                    Queremos ser un líder global en fragancias,
-                    exportando sus productos al extranjero. A futuro, desea
-                    mantenerse fiel a sus valores de innovación, integridad,
-                    transparencia, honestidad, enfoque en el cliente y calidad.
-                    La visión es ser reconocida internacionalmente por priorizar
-                    el bienestar, la seguridad, la comodidad y la satisfacción
-                    de sus clientes.
+                    Queremos ser un líder global en fragancias, exportando
+                    nuestros productos al extranjero. En el año 2030, deseamos
+                    mantenersnos fiel a nuestros valores de innovación,
+                    integridad, transparencia, honestidad, enfoque en el cliente
+                    y calidad. La visión es ser reconocidos internacionalmente
+                    por priorizar el bienestar, la seguridad, la comodidad y la
+                    satisfacción de nuestros clientes.
                 </p>
             </header>
 
-            <img className="aspect-video w-full p-8 lg:w-1/2"
+            <img
+                className="aspect-video w-full p-8 lg:w-1/2"
                 src="/mujerEnELCampo.jpg"
                 alt="imagen de una mujer en un campo"
             />
