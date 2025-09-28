2d7b4d59ee81ef5d309b5cfd753fbb36f53d6f1d
luifer991 <luifer991@gmail.com>
2025-09-28 14:19:36 -0500

    refactor: reutilze funciton useFetchProducts

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -2,17 +2,19 @@ import api from '../api/zorvanz-api'
 import { useQuery } from '@tanstack/react-query'
 import { PaginatedResponse, Product } from '../types/interfaces'
 
-async function fetchProducts() {
+async function fetchProducts(pageSize: number): Promise<Product[]> {
     const { data } = await api.get<PaginatedResponse>(
-        '/Product?pageNumber=1&pageSize=4',
+        `/Product?pageNumber=1&pageSize=${pageSize}`,
     )
     return data.data
 }
 
-export function useFetchProducts() {
+export function useFetchProducts(pageSize: number) {
     return useQuery({
-        queryKey: ['productData'],
-		queryFn: fetchProducts,
+        queryKey: ['productData', pageSize],
+		queryFn: () => fetchProducts(pageSize),
+
+		enabled: pageSize > 0,
     })
 }
 
