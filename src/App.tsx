08899ca9f1e091ca19e4ad6764046f8dd6b98442
luifer991 <luifer991@gmail.com>
2025-10-03 16:07:13 -0500

    refactor: protecting router to handling perision to access to a component

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,14 @@ import Dashboard from './Auth/Dashboard.tsx'
 import Register from './Auth/Register.tsx'
 import RegisterAdmin from './Auth/RegisterAdmin.tsx'
 import ProductUpdateForm from './features/products/UI/UpdateProductForm.tsx'
+import SuccessEmailVerified from './SuccessEmailVerified.tsx'
+import { ProtectedRoute } from './UI/ProtectedRoute.tsx'
+import { useAuthStore } from './store/auth'
+import { Role } from './types/interfaces'
 
 function App() {
+    const user = useAuthStore((state) => state.profile)
+    const isAuth = useAuthStore((state) => state.isAuth)
     return (
         <Routes>
             <Route index element={<Home />} />
@@ -23,12 +29,50 @@ function App() {
             <Route path="/cart" element={<ShoppingCart />} />
             <Route path="/product-catalog/:id" element={<ProductInfo />} />
             <Route path="/add-product" element={<ProducForm />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route
+                path="/profile"
+                element={
+                    <ProtectedRoute
+                        isAuth={isAuth}
+                        user={user!}
+                        allowedRoles={[Role.User]}
+                    >
+                        <Profile />
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                path="/dashboard"
+                element={
+                    <ProtectedRoute
+                        isAuth={isAuth}
+                        user={user!}
+                        allowedRoles={[Role.Admin]}
+                    >
+                        <Dashboard />
+                    </ProtectedRoute>
+                }
+            />
             <Route path="/register" element={<Register />} />
-            <Route path="/register-admin" element={<RegisterAdmin />} />
+            <Route
+                path="/register-admin"
+                element={
+                    <ProtectedRoute
+                        isAuth={isAuth}
+                        user={user!}
+                        allowedRoles={[Role.Admin]}
+                    >
+                        <RegisterAdmin />
+                    </ProtectedRoute>
+                }
+            />
             <Route path="/update-product/:id" element={<ProductUpdateForm />} />
             <Route path="*" element={<ErrorPage />} />
+            <Route path="/verified-email" element={<SuccessEmailVerified />} />
+            <Route
+                path="/verified-email/:token"
+                element={<SuccessEmailVerified />}
+            />
         </Routes>
     )
 }
