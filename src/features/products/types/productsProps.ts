d9f448d96e40c9b17cd43b480e742b5c964b462c
luifer991 <luifer991@gmail.com>
2025-10-03 16:05:04 -0500

    put props into a new file

--- src/features/products/types/productsProps.ts
+++ src/features/products/types/productsProps.ts
@@ -3,3 +3,12 @@ import { Product } from "./productTypes";
 export interface CartButtonProps {
     product: Product
 }
+
+
+export interface ModalProps {
+    productName: string
+    id: string
+    onMutate: (productId: string) => void // Función para ejecutar la mutación (mutate)
+    isPending: boolean // Estado de carga (isPending)
+    onSuccess: boolean // Función para manejar el éxito de la mutación (onSuccess)
+}
