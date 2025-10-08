f343d9809b2c8bb84d8c2492207138db59d833d8
LuiferDev <luifer991@gmail.com>
2025-10-07 23:15:38 -0500

    change mission description to the real version

--- src/about/UI/Mission.tsx
+++ src/about/UI/Mission.tsx
@@ -6,7 +6,9 @@ export default function Mission() {
                 <img className="aspect-video w-full p-4 mt-8 lg:w-1/2 lg:mt-0" src="/mission.jpg" alt="imagen de una fragancia" />
                 <header className="m-20 flex flex-col justify-center items-center text-start gap-4">
                     <h2 className="pb-4 text-5xl w-full">Misión</h2>
-                    <p className="text-xl w-[20ch] lg:w-[40ch]">Queremos ser un líder en la industria de las fragancias, ofreciendo productos que se enfocan en darle bienestar a los usuarios. Se enfocan en crear aromas que no solo agraden a los sentidos, sino que también promuevan el bienestar de los usuarios, buscando inspirar amor y lealtad en cada experiencia con sus fragancias.</p>
+                    <p className="text-xl w-[20ch] lg:w-[40ch]">
+En Zorvanz nos dedicamos a fabricar y comercializar velas y ambientadores personalizados que transforman los espacios en lugares agradables, y que representan la esencia de cada uno de nuestros clientes. Nos esforzamos por crear productos con fragancias únicas, elaborados con ingredientes amigables y respetuosos con quienes los usan.
+          </p>
                 </header>
             </div>
 
