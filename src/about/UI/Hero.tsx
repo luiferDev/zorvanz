b572780bbe1659efa9b41156d88f68e17e896aff
LuiferDev <luifer991@gmail.com>
2025-10-08 00:24:05 -0500

    change hero styles

--- src/about/UI/Hero.tsx
+++ src/about/UI/Hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
     return (
         <section className="flex flex-col justify-center items-center lg:flex-row lg:h-screen">
             <header className="w-full pt-20 px-6 flex flex-col justify-center items-center m-8 gap-4 lg:w-1/3 lg:p-30">
-                <h1 className="pb-4 text-6xl lg:ml-[-3rem]">Nuestra Historia</h1>
+                <h1 className="pb-4 text-4xl lg:ml-[-3rem]">Nuestra Historia</h1>
                 <p className="text-xl w-[20ch] lg:w-[40ch]">
                     Zorvanz es más que una marca de fragancias; es una invitación a descubrir tu esencia única. Desde nuestros inicios, nos hemos dedicado a crear experiencias olfativas personalizadas que reflejan la individualidad de cada persona.
                 </p>
